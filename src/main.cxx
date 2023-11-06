2135570f26fa4360dc15259f8940836571e21382
Pavel Kalabin <kalabin.pp@edu.spbstu.ru>
2023-11-06 23:28:25 +0300

    added simple config

--- src/main.cxx
+++ src/main.cxx
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+void usage()
+{
+	printf("tasm-makeconfig <path>");
+}
+
+void write_config(std::ofstream& file, const std::string path)
+{
+	file.open(path, std::ios::out);
+	file << "hello, world\n";
+}
 
 int main(int argc, char* argv[])
 {
+	if (argc < 2)
+	{
+		usage();
+	}	
+	std::ofstream config;
+	//std::string path = "/home/zafod/Documents/DOS_ASM/tasm-mkconfig/config.conf";
+	std::string path = argv[1];
+	write_config(config, path);
 	std::cout << "hello, world!\n";
 	return 0;
 }
