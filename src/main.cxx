6e8ccd93150b3407768a6b9f0b74a1ba1dff80fa
Pavel Kalabin <kalabin.pp@edu.spbstu.ru>
2023-11-06 23:47:35 +0300

    actual want to sleep

--- src/main.cxx
+++ src/main.cxx
@@ -4,10 +4,20 @@
 
 void usage()
 {
-	printf("tasm-makeconfig <path>");
+	printf("Usage: tasm-makeconfig <output path> <tasm path> <source path> <filename>\n");
+	printf("\toutput path\t- path/to/dir where you want to see config file\n");
+	printf("\ttasm path\t- path/to/dir where tasm binutils is located\n");
+	printf("\tsource path\t- path/to/dir where asm source is located\n");
+	printf("\tfilename\t- name of asm source file\n");
 }
 
-void write_config(std::ofstream& file, const std::string path)
+void write_config(
+		std::ofstream& file, 
+		const std::string path, 
+		const std::string tasmPath, 
+		const std::string sourcePath,
+		const std::string filename
+		)
 {
 	file.open(path, std::ios::out);
 	if (!file.is_open())
@@ -15,18 +25,30 @@ void write_config(std::ofstream& file, const std::string path)
 		std::cerr << "File not found!\n";
 		exit(1);
 	}
-	file << "hello, world\n";
+	file << "[autoexec]\n";
+	file << "@echo off";
+	file << "mount c: " << tasmPath << '\n';
+	file << "mount d: " << sourcePath << '\n';
+	file << "path=z:\\;c:\\\n";
+	file << "d:\n";
+	file << "cls\n";
+	file << "d:\\" << filename << '\n';
+	file << "pause\n";
+	file << "exit\n";
 }
 
 int main(int argc, char* argv[])
 {
-	if (argc != 2)
+	if (argc != 5)
 	{
 		usage();
 	}	
 	std::ofstream config;
 	//std::string path = "/home/zafod/Documents/DOS_ASM/tasm-mkconfig/config.conf";
 	std::string path = argv[1];
+	std::string tasmPath = argv[2];
+	std::string sourcePath = argv[3];
+	std::string filename = argv[4];
 	write_config(config, path);
 	std::cout << "hello, world!\n";
 	return 0;
